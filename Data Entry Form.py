5dfb1ebc0babf1c509f0e728bd0e903bf3ef1c3f
Njoguu <njoguuallan@gmail.com>
2021-02-07 12:13:37 +0300

    Script with Window and few Data Entry spaces

---
+++ Data Entry Form.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+class Registration:
+
+    def __init__(self,reg):
+        self.lbl1 = Label(reg, text="First Name:", font=("Arial Black", 8))
+        self.lbl2 = Label(reg, text="Second Name:", font=("Arial Black", 8))
+        self.lbl3 = Label(reg, text="Age:", font=("Arial Black", 8))
+        self.lbl4 = Label(reg, text="Adm Number:", font=("Arial Black", 8))
+        self.entr1 = Entry(fg='black', bg='white', bd=5)
+        self.entr2 = Entry(fg='black', bg='white', bd=5)
+        self.entr3 = Entry(fg='black', bg='white', bd=5)
+        self.entr4 = Entry(fg='black', bg='white', bd=5)
+        self.lbl1.place(x=150, y=50)
+        self.entr1.place(x=250, y=50)
+        self.lbl2.place(x=450, y=50)
+        self.entr2.place(x=550, y=50)
+        self.lbl3.place(x=150, y=100)
+        self.entr3.place(x=250, y=100)
+        self.lbl4.place(x=150, y=150)
+        self.entr4.place(x=250, y=150)
+
+
+
+
+
+
+
+
+
+
+
+
+window=Tk()
+window.title("Student Registration")
+mywin=Registration(window)
+lbl=Label(window, text="STUDENT REGISTRATION", fg='black', font=("Arial Black", 16))
+lbl.place(x=0,y=0)
+window.geometry("900x700+10+20")
+window.mainloop()
+
